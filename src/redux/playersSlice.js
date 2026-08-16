260e66153ebcada92f01a763840ed9901ed1729e
a81189840-cmyk <a81189840@gmail.com>
2026-08-16 14:08:23 +0530

    Addict

--- src/redux/playersSlice.js
+++ src/redux/playersSlice.js
@@ -1,53 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
-
-const initialState = {
-    players: [],
-};
-
-const playerSlice = createSlice({
-    name: "player",
-
-    initialState,
-
-    reducers: {
+const initialState = {players: [],};
+const playerSlice = createSlice({name: "player",initialState,reducers: {
         setPlayers: (state, action) => {
             state.players = action.payload;
         },
-
         addPlayer: (state, action) => {
             state.players.push(action.payload);
         },
-
         updatePlayer: (state, action) => {
             const { id, data } = action.payload;
-
             const player = state.players.find(
                 (player) => player.id === id
             );
-
             if (player) {
                 Object.assign(player, data);
             }
         },
-
         removePlayer: (state, action) => {
             state.players = state.players.filter(
                 (player) => player.id !== action.payload
             );
         },
-
         clearPlayers: (state) => {
             state.players = [];
         },
     },
 });
-
-export const {
-    setPlayers,
-    addPlayer,
-    updatePlayer,
-    removePlayer,
-    clearPlayers,
-} = playerSlice.actions;
-
+export const {setPlayers,addPlayer,updatePlayer,removePlayer,clearPlayers,} = playerSlice.actions;
 export default playerSlice.reducer;
--- src/redux/quizSlice.js
+++ src/redux/quizSlice.js
@@ -1,33 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 const initialState = {quizId: "",title: "",questions: [],};
 const quizSlice = createSlice({
-  name: "quiz",
-  initialState,
-  reducers: {
+  name: "quiz",initialState,reducers: {
     setQuiz: (state, action) => {state.quizId = action.payload.quizId;state.title = action.payload.title;state.questions = action.payload.questions;
     },
-
     addQuestion: (state, action) => {
       state.questions.push(action.payload);
     },
-
     removeQuestion: (state, action) => {
       state.questions.splice(action.payload, 1);
     },
-
-    clearQuiz: (state) => {
-      state.quizId = "";
+    clearQuiz: (state) => {state.quizId = "";
       state.title = "";
       state.questions = [];
     },
   },
 });
-
-export const {
-  setQuiz,
-  addQuestion,
-  removeQuestion,
-  clearQuiz,
-} = quizSlice.actions;
-
+export const {setQuiz,addQuestion,removeQuestion,clearQuiz,} = quizSlice.actions;
 export default quizSlice.reducer;
